e2543da3f13d24690e66143414bea77708766e49
Agustin Hernandez <mmmunapregunta@gmail.com>
2022-10-24 16:44:16 -0500

    asincronismo agregado

--- main.js
+++ main.js
@@ -5,19 +5,13 @@ const URL = 'https://api.thecatapi.com/v1/images/search'
 myButton.addEventListener('click', kittyRecharger);
 
 
-function kittyRecharger() {
-    try {
-        fetch(URL)
-        .then(res => res.json())
-        .then(data => {
-            const img = document.querySelector('img');
-            img.src = data[0].url;
-        })
-    }
-    catch(error) {
-        console.log(error);
-    }
-    
+async function kittyRecharger() {
+    const res = await fetch(URL);
+    const data = await res.json();
+
+    const img = document.querySelector('img');
+    img.src = data[0].url;
 }
 
 kittyRecharger();
+
