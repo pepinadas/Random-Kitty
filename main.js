c71525a1d8f6c6efbaad8da5e4ec3e9495f2678f
Agustin Hernandez <mmmunapregunta@gmail.com>
2022-10-24 14:54:37 -0500

    Alggunos problemas arreglados y boton funcionando

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
-console.log('Hello world');
+const myButton = document.querySelector("button");
 
 const URL = 'https://api.thecatapi.com/v1/images/search'
 
-fetch(URL)
-    .then(res => res.json())
-    .then(data => {
-        const img = document.querySelector('img');
-        img.src = data[0].url;
-    })
+myButton.addEventListener('click', kittyRecharger);
+
+
+function kittyRecharger() {
+    try {
+        fetch(URL)
+        .then(res => res.json())
+        .then(data => {
+            const img = document.querySelector('img');
+            img.src = data[0].url;
+        })
+    }
+    catch(error) {
+        console.log(error);
+    }
+    
+}
+
+kittyRecharger();
